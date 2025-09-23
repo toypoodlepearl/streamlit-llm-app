cd9c230d45a59ed647791635901ad71a79ec0021
yoichiro yano <toypoodlepearl741@gmail.com>
2025-09-24 01:02:49 +0900

    first commit.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pip
 import streamlit as st
-pip install langchain==0.3.0 openai==1.47.0 langchain-community==0.3.0 langchain-openai==0.2.2 httpx==0.27.2
-pip install python-dotenv
+##  pip install langchain==0.3.0 openai==1.47.0 langchain-community==0.3.0 langchain-openai==0.2.2 httpx==0.27.2
+##  pip install python-dotenv
 
 from dotenv import load_dotenv
 load_dotenv()
